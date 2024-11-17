348a7748c2b2b142bff34666daf751174070c254
Yuma Antoine Decaux <yuma@astreos.space>
2024-11-17 13:57:14 +1000

    date formatter set to yyyy-MM-dd HH:mm:ss

--- Sources/AOSUniverse/AOSUniverse+IO.swift
+++ Sources/AOSUniverse/AOSUniverse+IO.swift
@@ -63,7 +63,7 @@ internal func moveFileToPath(assetpath: [String], type: String, url: URL, text: 
 
 public func getGmtDateFormatter() -> DateFormatter {
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "YYYY-MM-DD HH:MM:SS"
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
     dateFormatter.locale = Locale(identifier: "en_US_POSIX")
     dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
     return dateFormatter
