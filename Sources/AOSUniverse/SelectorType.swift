40b592f3f184466b0f4aa28a227c1b78c2c484e9
Yuma Antoine Decaux <yuma@astreos.space>
2025-02-16 16:20:05 +1000

    use idx for SelectorType as dict does not hold rference

--- Sources/AOSUniverse/SelectorType.swift
+++ Sources/AOSUniverse/SelectorType.swift
@@ -11,8 +11,16 @@ case selector
 case confirmedExoPlanet
 case ticCandidate
 
-
 public var id:String {
 return self.rawValue
 }
+    
+    
+    public var idx:Int {
+        switch self {
+        case .selector: return 0
+        case .confirmedExoPlanet: return 1
+        case .ticCandidate: return 2
+        }
+    }
 }
