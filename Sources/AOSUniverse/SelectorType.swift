a7a23a78f294f9844407f03ab44432d3a7e673b4
Yuma Antoine Decaux <yuma@astreos.space>
2025-09-14 12:35:21 +1000

    added new scn based icons for selectors

--- Sources/AOSUniverse/SelectorType.swift
+++ Sources/AOSUniverse/SelectorType.swift
@@ -7,9 +7,16 @@
 
 
 public enum SelectorType:String, Codable, Identifiable, CaseIterable {
-case selector
-case confirmedExoPlanet
-case ticCandidate
+    case exo_question
+    case exo
+    case sat1_polygon
+    case sat2_square
+    case sat3_polygon_lines
+    case selector
+    case simple_circle
+    case star_6_cross_4lines
+    case star4_5lines_circle
+    case star4lines_circle
 
 public var id:String {
 return self.rawValue
@@ -18,9 +25,16 @@ return self.rawValue
     
     public var idx:Int {
         switch self {
-        case .selector: return 0
-        case .confirmedExoPlanet: return 1
-        case .ticCandidate: return 2
+        case .exo_question: return 0
+        case .exo: return 1
+        case .sat1_polygon: return 2
+        case .sat2_square: return 3
+        case .sat3_polygon_lines: return 4
+        case .selector: return 5
+        case .simple_circle: return 6
+        case .star_6_cross_4lines: return 7
+        case .star4_5lines_circle: return 8
+        case .star4lines_circle: return 9
         }
     }
 }
